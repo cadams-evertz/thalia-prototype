e18f1f9ae95e9f7dc1a42ec979741e1f7cad7519
Chris Adams <cadams@evertz.com>
2025-02-03 12:35:50 -0500

    -ShellTask tweaks

--- src/task/shell/shell-task.ts
+++ src/task/shell/shell-task.ts
@@ -1,25 +1,56 @@
 import * as thl_fs from '../../fs';
+import * as thl_log from '../../log';
 import * as thl_process from '../../process';
 import * as thl_task from '..';
+import * as thl_text from '../../text';
 import * as thl_util from '../../util';
 
 export class ShellTask extends thl_task.FilesProviderTask<ShellTask.Options> {
+  private cmdFilename: thl_fs.Path;
+  private commands: string[] = [];
   private inputs: thl_fs.Path[] = [];
 
   constructor(options: ShellTask.Options) {
     const dependencies = thl_task.Task.filterArray(thl_util.ensureArray(options.input ?? options.inputs));
     super(options, dependencies);
     this.outputs = thl_task.BuildDir.asBuildPathArray(thl_util.ensureArray(options.output ?? options.outputs));
+    this.cmdFilename = this.outputs[0].append('.cmd');
   }
 
   protected override prepare(): void {
     this.inputs = thl_task.FilesProviderTask.toPaths(thl_util.ensureArray(this.options.input ?? this.options.inputs));
+
+    const substitutions = {
+      ...this.generateSubstitutions('input', this.inputs),
+      ...this.generateSubstitutions('output', this.outputs),
+      ...this.options.substitutions,
+    };
+
+    this.commands = this.options.commands.map(command => thl_text.expandTemplate(command, substitutions));
   }
 
   protected override needToRun(): boolean {
-    return this.inputs.length === 0 || this.outputs.length === 0
-      ? true
-      : thl_fs.file.isNewer(this.inputs, this.outputs);
+    if (this.options.needToRun) {
+      const needToRun = this.options.needToRun(this.inputs, this.outputs);
+
+      if (needToRun !== undefined) {
+        return needToRun;
+      }
+    }
+
+    if (this.inputs.length === 0 || this.outputs.length === 0) {
+      return true;
+    }
+
+    const commandDifferent = this.cmdFilename.exists()
+      ? thl_fs.file.readText(this.cmdFilename) !== this.commands.join('\n')
+      : true;
+
+    if (commandDifferent) {
+      return true;
+    }
+
+    return thl_fs.file.isNewer(this.inputs, this.outputs);
   }
 
   protected override async run(taskRunnerOptions?: thl_task.TaskRunner.Options): Promise<void> {
@@ -29,40 +60,39 @@ export class ShellTask extends thl_task.FilesProviderTask<ShellTask.Options> {
       thl_fs.dir.createForFile(output);
     }
 
-    for (const command of this.options.commands) {
-      const resolvedCommand = this.resolveCommand(
-        this.resolveCommand(command, 'input', this.inputs),
-        'output',
-        this.outputs,
-      );
-      thl_process.execute(resolvedCommand, { echoCommand: !!taskRunnerOptions?.debug });
+    for (const command of this.commands) {
+      await thl_process.executeAsync(command, {
+        echoCommand: this.options.echoCommand ?? !!taskRunnerOptions?.debug,
+      });
     }
+
+    thl_log.setOptionsWhile({ action: false }, () => {
+      thl_fs.file.writeText(this.cmdFilename, this.commands.join('\n'));
+    });
   }
 
-  private resolveCommand(command: string, prefix: string, items: thl_fs.Path[]): string {
-    let resolvedCommand = command.replace(new RegExp(`\\{\\{${prefix}s\\}\\}`, 'g'), items.join(' '));
-
-    if (items.length > 0) {
-      resolvedCommand = resolvedCommand.replace(new RegExp(`\{\{${prefix}\}\}`, 'g'), items[0].absolute());
-
-      for (let index = 0; index < items.length; index++) {
-        resolvedCommand = resolvedCommand.replace(
-          new RegExp(`\\{\\{${prefix}${index}\\}\\}`, 'g'),
-          items[index].absolute(),
-        );
-      }
-    }
-
-    return resolvedCommand;
+  private generateSubstitutions(prefix: string, items: thl_fs.Path[]): Record<string, string> {
+    return items.length === 0
+      ? {
+          [`${prefix}s`]: '',
+        }
+      : {
+          [`${prefix}s`]: items.join(' '),
+          [prefix]: items[0].absolute(),
+          ...Object.fromEntries(items.map((item, index) => [`${prefix}${index}`, item.absolute()])),
+        };
   }
 }
 
 export namespace ShellTask {
   export interface Options extends thl_task.Task.Options {
     commands: string[];
+    echoCommand?: boolean;
     input?: thl_task.FilesProviderTasklike;
     inputs?: thl_task.FilesProviderTasklike[];
+    needToRun?: (inputs: thl_fs.Path[], outputs: thl_fs.Path[]) => boolean | undefined;
     output?: thl_fs.Pathlike;
     outputs?: thl_fs.Pathlike[];
+    substitutions?: Record<string, unknown>;
   }
 }
